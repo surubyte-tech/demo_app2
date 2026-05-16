194fa8a84f027014a5dbe3c1dec5d6b7fa451fa0
sooraj1312 <soorajxaviar0@gmail.com>
2026-05-16 10:23:42 +0530

    initial commite branch3

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h1>this is branch3</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
